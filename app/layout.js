c7b5f11be3a234a0d5380307f2d1bbf1c3912111
masood ahmadi <masoodahmadi553@gmail.com>
2025-12-14 01:52:27 +0200

    navbar changed

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './globals.css';
 import Providers from './providers';
 
 export default function RootLayout({ children }) {
--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
 
 
   return (
-    <div>
+    <div className='background-color'>
       <Navbar />
       <Carousels size={size} />
 
